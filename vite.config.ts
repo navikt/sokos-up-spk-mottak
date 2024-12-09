f8901c64843087bdb47c9bf44e0d4ca1d85f6da6
takwaiwang <tak.wai.wang@nav.no>
2024-12-09 09:42:52 +0100

    oppdater deploy script og fiks vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,58 +11,62 @@ const reactUrl = "https://www.nav.no/tms-min-side-assets/react/18/esm/index.js";
 const reactDomUrl =
   "https://www.nav.no/tms-min-side-assets/react-dom/18/esm/index.js";
 
-export default defineConfig(({ mode }) => ({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/App.tsx"),
-      name: "sokos-up-spk-mottak",
-      formats: ["es"],
-      fileName: () => "bundle.js",
+export default defineConfig(({ mode }) => {
+  const proxyConfig = {
+    ...((mode === "backend" || /^.*-q1$/.test(mode)) && {
+      "/spk-mottak-api/api/v1": {
+        target: /^.*-q1$/.test(mode)
+          ? "https://sokos-spk-mottak.intern.dev.nav.no"
+          : "http://localhost:8080",
+        rewrite: (path: string) => path.replace(/^\/spk-mottak-api/, ""),
+        changeOrigin: true,
+        secure: /^.*-q1$/.test(mode),
+      },
+    }),
+  };
+
+  return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/App.tsx"),
+        name: "sokos-up-spk-mottak",
+        formats: ["es"],
+        fileName: () => "bundle.js",
+      },
     },
-  },
-  css: {
-    modules: {
-      generateScopedName: "[name]__[local]___[hash:base64:5]",
+    css: {
+      modules: {
+        generateScopedName: "[name]__[local]___[hash:base64:5]",
+      },
     },
-  },
-  server: {
-    proxy: {
-      ...(mode === "backend" && {
-        "/spk-mottak-api/api/v1": {
-          target: /^.*-q1$/.test(mode)
-            ? "https://sokos-spk-mottak.intern.dev.nav.no"
-            : "http://localhost:8080",
-          rewrite: (path: string) => path.replace(/^\/spk-mottak-api/, ""),
-          changeOrigin: true,
-          secure: /^.*-q1$/.test(mode),
-        },
+    server: {
+      proxy: proxyConfig,
+    },
+    plugins: [
+      react(),
+      cssInjectedByJsPlugin(),
+      EnvironmentPlugin({
+        NODE_ENV: process.env.NODE_ENV || "development",
       }),
-    },
-  },
-  plugins: [
-    react(),
-    cssInjectedByJsPlugin(),
-    EnvironmentPlugin({
-      NODE_ENV: process.env.NODE_ENV || "development",
-    }),
-    viteMockServe({
-      mockPath: "mock",
-      enable: mode === "local-mock",
-    }),
-    {
-      ...importMapPlugin({
-        maps: [
-          {
-            imports: {
-              react: reactUrl,
-              "react-dom": reactDomUrl,
+      viteMockServe({
+        mockPath: "mock",
+        enable: mode === "local-mock",
+      }),
+      {
+        ...importMapPlugin({
+          maps: [
+            {
+              imports: {
+                react: reactUrl,
+                "react-dom": reactDomUrl,
+              },
             },
-          },
-        ],
-      }),
-      enforce: "pre",
-      apply: "build",
-    },
-    terser(),
-  ],
-}));
+          ],
+        }),
+        enforce: "pre",
+        apply: "build",
+      },
+      terser(),
+    ],
+  };
+});
